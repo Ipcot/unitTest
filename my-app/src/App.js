f7088b8e33deaefcdf981f35537a31c39e5a83c3
Yuriy Pokhyl <yuriypokhyl@Yuriys-Air.Dlink>
2022-10-29 22:38:41 +0300

    add profile

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Profile } from './component/Profile';
+import user from './data/user.json'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatr}
+    stats={user.stats}
+    />
   );
 }
 
---
+++ my-app/src/component/Profile.js
@@ -0,0 +1,13 @@
+
+export const Profile = ({
+    username,
+    tag,
+    location,
+    avatar,
+    satus: {followers, views, likes},
+}) => {
+
+    return (
+        <div></div>
+    )
+}
